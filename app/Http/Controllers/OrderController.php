c7947007a4f62e38e70667b43d1170fa5d5d0604
Hasin Hayder <countdraculla@gmail.com>
2023-10-10 10:34:07 +0600

    Code cleanup

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,19 +7,11 @@ use App\Models\Order;
 
 class OrderController extends Controller
 {
-    function addProductsToOrder(Request $request)
-    {
-        $order = $request->order;
-        $products = $request->products;
-        $order->products()->attach($products);
 
-        return $order;
-    }
 
     function createOrder(Request $request)
     {
         $user = $request->user();
-       //create new Order
         $order = new Order();
         $order->user_id = $user->id;
         $order->total_amount = $request->total_amount;
@@ -30,21 +22,6 @@ class OrderController extends Controller
         return $order;
     }
 
-    function createNewOrderWithProducts(Request $request){
-        $user = auth()->user();
-        $order = new Order();
-        $order->user_id = $user->id;
-        $order->order_number = '123456789';
-        $order->total_amount = 100;
-        $order->save();
-
-        //now attach products to this order
-        $products = $request->products;
-        $order->products()->attach($products);
-
-        return $order;
-    }
-
     function getUserOrders(){
         $user = auth()->user();
         $orders = $user->orders()->with('products')->get();
@@ -52,7 +29,7 @@ class OrderController extends Controller
     }
 
     function getOrderDetails($id, Request $request){
-        $order = Order::find($id);
+        $order = Order::findOrFail($id);
         $order->products;
         return $order;
     }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -20,19 +20,7 @@ class ProductController extends Controller
         //     Cache::put('products', $products, 3600);
         //     //return $products;
         // }
-        //now enter the products into the database from the following json
-        // {
-        //     "id": 2,
-        //     "title": "Mens Casual Premium Slim Fit T-Shirts ",
-        //     "price": 22.3,
-        //     "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-        //     "category": "men's clothing",
-        //     "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-        //     "rating": {
-        //     "rate": 4.1,
-        //     "count": 259
-        //     }
-        //     }
+
 
         // foreach($products as $product){
         //     $product = [
@@ -56,13 +44,5 @@ class ProductController extends Controller
     {
         $product = Product::find($id);
         return $product;
-        // $productCache = Cache::get('product' . $id);
-        // if ($productCache) {
-        //     return $productCache;
-        // } else {
-        //     $product = Http::get('https://fakestoreapi.com/products/' . $id)->json();
-        //     Cache::put('product' . $id, $product, 3600);
-        //     return $product;
-        // }
     }
 }
